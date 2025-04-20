a65d7cdb77bb1628a5726d305fc546be5315a78b
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-04-20 18:42:39 -0400

    Added pseudocode structure

---
+++ script.js
@@ -0,0 +1,52 @@
+// Variables
+
+// Constructors
+
+// Event Handlers
+
+// Functions
+    // Countdown Timer Display
+        // Start at 25:00 minutes
+        // After hitting 0, change display to show "Time for a break! :)"
+        // After 10 minutes, reset display to 25:00 minutes
+
+    // Start Timer
+        // Begin countdown from current remaining time
+
+    // Pause Timer
+        // See if there is a built in way to pause
+            // IF NOT, get time remaining when user clicked button
+                // Store in a variable
+                // Display value of the variable
+                // Start a new countdown with the time remaining
+
+    // Stop Timer
+        // Reset timer to 25:00 minutes
+
+    // Session Counter
+        // When timer reaches 0:00, add 1 to counter
+        // Log Session Counter
+
+    // Chosen Mood Button
+        // Change Button Color 
+        // Log Chosen Mood
+
+    // Mood Notes
+        // Display empty block with a + sign
+            // When clicked, accept user text
+        // Display input text
+        // Move + block under input text and accept another input
+        // Ability to scroll if user inputs exceed view space
+        // Log all input texts
+    
+    // Daily Habits Buttons
+        // If + is hit, add 1 to counter
+        // If - is hit, subtract 1 from counter
+        // Log counter totals
+
+    // To-do List
+        // Display + in an empty block
+            // When clicked, allow user input
+            // Display user input
+            // Move + block below user input
+            // Allow scroll if inputs exceed view area
