602803faf87738d66e1b1fa8dbb73a562c12c237
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-04-21 07:59:29 -0400

    Added timer function and event handlers

--- script.js
+++ script.js
@@ -1,29 +1,76 @@
 // Variables
+let timerCounter = 0;
+let timeRemaining = 1500;
 
 // Selectors
+const timerDisplay = document.querySelector("#countdownTimer");
+const timerCountDisplay = document.querySelector("#sessionDisplay");
 const startButton = document.querySelector("#startTimer");
 const pauseButton = document.querySelector("#pauseButton");
 const stopButton = document.querySelector("#stopButton");
 
+const moodHappy = document.querySelector(".happy");
+const moodIndifferent = document.querySelector(".indifferent");
+const moodSad = document.querySelector(".sad");
+const moodAngry = document.querySelector(".angry");
+const moodFearful = document.querySelector(".fear");
+
+const dailyWalk = document.querySelector(".walk");
+const dailyYoga = document.querySelector(".yoga");
+const dailyChores = document.querySelector(".chores");
+
 // Event Handlers
-startButton.addEventListener('click', () => log('Start Timer Button'));
+startButton.addEventListener('click', () => startTimer(timeRemaining)); // Set to 1500 when done testing
 pauseButton.addEventListener('click', () => log('Pause Timer Button'));
 stopButton.addEventListener('click', () => log('Stop Timer Button'));
+
+moodHappy.addEventListener('click', () => log('Happy Face Button'));
+moodIndifferent.addEventListener('click', () => log('Indifferent Face Button'));
+moodSad.addEventListener('click', () => log('Sad Face Button'));
+moodAngry.addEventListener('click', () => log('Angry Face Button'));
+moodFearful.addEventListener('click', () => log('Fear Face Button '));
 
 // Functions
 function log(clicked) {
     console.log(`Clicked ${clicked}`);
-}
+};
 
+function startTimer(duration) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer /60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        timerDisplay.textContent =  minutes + ":" + seconds;
+        if (--timer < 0) {
+            timer = duration;
+        } else if (timer == 0) {
+            timerDisplay.textContent = "Time for a break!"
+            timerCounter++;
+            timerCountDisplay.textContent = timerCounter;
+        }
+    }, 1000)
+};
+
+// TO DO LIST
   /* Add all logs to the firebase database, saved per user */
+    // Change startTimer(duration) to timer(function,duration) where function is
+        // start 
+            // if function == start, begin timer from duration
+        // stop 
+            // if function == stop, remove current time, reset to 25:00
+        // pause 
+            // if function == pause, stop timer and update display to show time remaining
+            // store timer duration in timeRemaining variable
+
 
     // Countdown Timer Display
-        // Start at 25:00 minutes
-        // After hitting 0, change display to show "Time for a break! :)"
-        // After 10 minutes, reset display to 25:00 minutes
+        // After 5 minute break, reset display to 25:00 minutes
 
     // Start Timer
-        // Begin countdown from current remaining time
+        // Begin countdown from current remaining time after a pause
 
     // Pause Timer
         // See if there is a built in way to pause
